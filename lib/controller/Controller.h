746a7c1db86992f6335de72501fd4827ba214c00
domas-cloud <lukassdomass@gmail.com>
2025-08-18 21:13:14 +0300

    Squashed 'src/' content from commit fcf9c13
    git-subtree-dir: src
    git-subtree-split: fcf9c1326ea802b8511f52dfcf4213446a8f50e0

---
+++ lib/controller/Controller.h
@@ -0,0 +1,11 @@
+void read_sensor_data();
+void load_sensors();
+
+extern float turning_angle;
+extern bool track_ready;
+
+extern float f_left, 
+    f_right, 
+    b_left, 
+    b_right, 
+    front;
